88e7d90c9c2ca3439b2d83e41e9a9b71c856a709
aladdin731 <aladdin731@hotmail.com>
2021-04-27 18:34:22 -0700

    add chart

--- country_time_series.js
+++ country_time_series.js
@@ -1,10 +1,433 @@
-let URL = "https://api.covid19api.com/total/country/usa/status/confirmed"
+let country_list = [
+    { name: 'USA', code: 'US' },
+    { name: 'Spain', code: 'ES' },
+    { name: 'Italy', code: 'IT' },
+    { name: 'France', code: 'FR' },
+    { name: 'Germany', code: 'DE' },
+    { name: 'UK', code: 'GB' },
+    { name: 'Turkey', code: 'TR' },
+    { name: 'Iran', code: 'IR' },
+    { name: 'Russia', code: 'RU' },
+    { name: 'Belgium', code: 'BE' },
+    { name: 'Brazil', code: 'BR' },
+    { name: 'Canada', code: 'CA' },
+    { name: 'Netherlands', code: 'NL' },
+    { name: 'Switzerland', code: 'CH' },
+    { name: 'Portugal', code: 'PT' },
+    { name: 'India', code: 'IN' },
+    { name: 'Ireland', code: 'IE' },
+    { name: 'Austria', code: 'AT' },
+    { name: 'Peru', code: 'PE' },
+    { name: 'Sweden', code: 'SE' },
+    { name: 'Japan', code: 'JP' },
+    { name: 'S. Korea', code: 'KR' },
+    { name: 'Chile', code: 'CL' },
+    { name: 'Saudi Arabia', code: 'SA' },
+    { name: 'Poland', code: 'PL' },
+    { name: 'Ecuador', code: 'EC' },
+    { name: 'Romania', code: 'RO' },
+    { name: 'Pakistan', code: 'PK' },
+    { name: 'Mexico', code: 'MX' },
+    { name: 'Denmark', code: 'DK' },
+    { name: 'Norway', code: 'NO' },
+    { name: 'UAE', code: 'AE' },
+    { name: 'Czechia', code: 'CZ' },
+    { name: 'Australia', code: 'AU' },
+    { name: 'Singapore', code: 'SG' },
+    { name: 'Indonesia', code: 'ID' },
+    { name: 'Serbia', code: 'RS' },
+    { name: 'Philippines', code: 'PH' },
+    { name: 'Ukraine', code: 'UA' },
+    { name: 'Qatar', code: 'QA' },
+    { name: 'Malaysia', code: 'MY' },
+    { name: 'Belarus', code: 'BY' },
+    { name: 'Dominican Republic', code: 'DO' },
+    { name: 'Panama', code: 'PA' },
+    { name: 'Finland', code: 'FI' },
+    { name: 'Colombia', code: 'CO' },
+    { name: 'Luxembourg', code: 'LU' },
+    { name: 'South Africa', code: 'ZA' },
+    { name: 'Egypt', code: 'EG' },
+    { name: 'Argentina', code: 'AR' },
+    { name: 'Morocco', code: 'MA' },
+    { name: 'Thailand', code: 'TH' },
+    { name: 'Algeria', code: 'DZ' },
+    { name: 'Moldova', code: 'MD' },
+    { name: 'Bangladesh', code: 'BD' },
+    { name: 'Greece', code: 'GR' },
+    { name: 'Hungary', code: 'HU' },
+    { name: 'Kuwait', code: 'KW' },
+    { name: 'Bahrain', code: 'BH' },
+    { name: 'Croatia', code: 'HR' },
+    { name: 'Iceland', code: 'IS' },
+    { name: 'Kazakhstan', code: 'KZ' },
+    { name: 'Uzbekistan', code: 'UZ' },
+    { name: 'Estonia', code: 'EE' },
+    { name: 'Iraq', code: 'IQ' },
+    { name: 'New Zealand', code: 'NZ' },
+    { name: 'Azerbaijan', code: 'AZ' },
+    { name: 'Slovenia', code: 'SI' },
+    { name: 'Lithuania', code: 'LT' },
+    { name: 'Armenia', code: 'AM' },
+    { name: 'Bosnia and Herzegovina', code: 'BA' },
+    { name: 'Oman', code: 'OM' },
+    { name: 'North Macedonia', code: 'MK' },
+    { name: 'Slovakia', code: 'SK' },
+    { name: 'Cuba', code: 'CU' },
+    { name: 'Hong Kong', code: 'HK' },
+    { name: 'Cameroon', code: 'CM' },
+    { name: 'Afghanistan', code: 'AF' },
+    { name: 'Bulgaria', code: 'BG' },
+    { name: 'Tunisia', code: 'TN' },
+    { name: 'Ghana', code: 'GH' },
+    { name: 'Ivory Coast', code: 'CI' },
+    { name: 'Cyprus', code: 'CY' },
+    { name: 'Djibouti', code: 'DJ' },
+    { name: 'Latvia', code: 'LV' },
+    { name: 'Andorra', code: 'AD' },
+    { name: 'Lebanon', code: 'LB' },
+    { name: 'Costa Rica', code: 'CR' },
+    { name: 'Niger', code: 'NE' },
+    { name: 'Burkina Faso', code: 'BF' },
+    { name: 'Albania', code: 'AL' },
+    { name: 'Kyrgyzstan', code: 'KG' },
+    { name: 'Nigeria', code: 'NG' },
+    { name: 'Bolivia', code: 'BO' },
+    { name: 'Guinea', code: 'GN' },
+    { name: 'Uruguay', code: 'UY' },
+    { name: 'Honduras', code: 'HN' },
+    { name: 'San Marino', code: 'SM' },
+    { name: 'Palestine', code: 'PS' },
+    { name: 'Malta', code: 'MT' },
+    { name: 'Taiwan', code: 'TW' },
+    { name: 'Jordan', code: 'JO' },
+    { name: 'Réunion', code: 'RE' },
+    { name: 'Georgia', code: 'GE' },
+    { name: 'Senegal', code: 'SN' },
+    { name: 'Mauritius', code: 'MU' },
+    { name: 'DRC', code: 'CD' },
+    { name: 'Montenegro', code: 'ME' },
+    { name: 'Isle of Man', code: 'IM' },
+    { name: 'Sri Lanka', code: 'LK' },
+    { name: 'Mayotte', code: 'YT' },
+    { name: 'Kenya', code: 'KE' },
+    { name: 'Vietnam', code: 'VN' },
+    { name: 'Guatemala', code: 'GT' },
+    { name: 'Venezuela', code: 'VE' },
+    { name: 'Mali', code: 'ML' },
+    { name: 'Paraguay', code: 'PY' },
+    { name: 'El Salvador', code: 'SV' },
+    { name: 'Jamaica', code: 'JM' },
+    { name: 'Tanzania', code: 'TZ' },
+    { name: 'Martinique', code: 'MQ' },
+    { name: 'Guadeloupe', code: 'GP' },
+    { name: 'Rwanda', code: 'RW' },
+    { name: 'Congo', code: 'CG' },
+    { name: 'Brunei', code: 'BN' },
+    { name: 'Somalia', code: 'SO' },
+    { name: 'Gibraltar', code: 'GI' },
+    { name: 'Cambodia', code: 'KH' },
+    { name: 'Madagascar', code: 'MG' },
+    { name: 'Trinidad and Tobago', code: 'TT' },
+    { name: 'Gabon', code: 'GA' },
+    { name: 'Myanmar', code: 'MM' },
+    { name: 'Ethiopia', code: 'ET' },
+    { name: 'Aruba', code: 'AW' },
+    { name: 'French Guiana', code: 'GF' },
+    { name: 'Monaco', code: 'MC' },
+    { name: 'Bermuda', code: 'BM' },
+    { name: 'Togo', code: 'TG' },
+    { name: 'Liechtenstein', code: 'LI' },
+    { name: 'Equatorial Guinea', code: 'GQ' },
+    { name: 'Liberia', code: 'LR' },
+    { name: 'Barbados', code: 'BB' },
+    { name: 'Sudan', code: 'SD' },
+    { name: 'Guyana', code: 'GY' },
+    { name: 'Zambia', code: 'ZM' },
+    { name: 'Cabo Verde', code: 'CV' },
+    { name: 'Cayman Islands', code: 'KY' },
+    { name: 'Bahamas', code: 'BS' },
+    { name: 'French Polynesia', code: 'PF' },
+    { name: 'Uganda', code: 'UG' },
+    { name: 'Maldives', code: 'MV' },
+    { name: 'Libya', code: 'LY' },
+    { name: 'Guinea-Bissau', code: 'GW' },
+    { name: 'Macao', code: 'MO' },
+    { name: 'Haiti', code: 'HT' },
+    { name: 'Syria', code: 'SY' },
+    { name: 'Eritrea', code: 'ER' },
+    { name: 'Mozambique', code: 'MZ' },
+    { name: 'Saint Martin', code: 'MF' },
+    { name: 'Benin', code: 'BJ' },
+    { name: 'Chad', code: 'TD' },
+    { name: 'Mongolia', code: 'MN' },
+    { name: 'Nepal', code: 'NP' },
+    { name: 'Sierra Leone', code: 'SL' },
+    { name: 'Zimbabwe', code: 'ZW' },
+    { name: 'Angola', code: 'AO' },
+    { name: 'Antigua and Barbuda', code: 'AG' },
+    { name: 'Eswatini', code: 'SZ' },
+    { name: 'Botswana', code: 'BW' },
+    { name: 'Timor-Leste', code: 'TL' },
+    { name: 'Belize', code: 'BZ' },
+    { name: 'New Caledonia', code: 'NC' },
+    { name: 'Malawi', code: 'MW' },
+    { name: 'Fiji', code: 'FJ' },
+    { name: 'Dominica', code: 'DM' },
+    { name: 'Namibia', code: 'NA' },
+    { name: 'Saint Lucia', code: 'LC' },
+    { name: 'Grenada', code: 'GD' },
+    { name: 'Saint Kitts and Nevis', code: 'KN' },
+    { name: 'CAR', code: 'CF' },
+    { name: 'St. Vincent Grenadines', code: 'VC' },
+    { name: 'Turks and Caicos', code: 'TC' },
+    { name: 'Falkland Islands', code: 'FK' },
+    { name: 'Greenland', code: 'GL' },
+    { name: 'Montserrat', code: 'MS' },
+    { name: 'Seychelles', code: 'SC' },
+    { name: 'Suriname', code: 'SR' },
+    { name: 'Nicaragua', code: 'NI' },
+    { name: 'Gambia', code: 'GM' },
+    { name: 'Vatican City', code: 'VA' },
+    { name: 'Mauritania', code: 'MR' },
+    { name: 'Papua New Guinea', code: 'PG' },
+    { name: 'St. Barth', code: 'BL' },
+    { name: 'Burundi', code: 'BI' },
+    { name: 'Bhutan', code: 'BT' },
+    { name: 'Caribbean Netherlands', code: 'BQ' },
+    { name: 'British Virgin Islands', code: 'VG' },
+    { name: 'Sao Tome and Principe', code: 'ST' },
+    { name: 'South Sudan', code: 'SD' },
+    { name: 'Anguilla', code: 'AI' },
+    { name: 'Saint Pierre Miquelon', code: 'PM' },
+    { name: 'Yemen', code: 'YE' },
+    { name: 'China', code: 'CN' }
+];
+
+const search_country_element = document.querySelector(".search-country");
+const country_list_element = document.querySelector(".country-list");
+const change_country_btn = document.querySelector(".change-country");
+const close_list_btn = document.querySelector(".close");
+const country_input = document.getElementById('search-input')
+const start_date_element = document.querySelector(".change-start-date");
+const end_date_element = document.querySelector(".change-end-date");
 
 
+let country_code = geoplugin_countryCode()
+let user_country;
+country_list.forEach((country) => {
+  if (country.code == country_code) {
+    user_country = country.name;
+  }
+});
 
-fetch(URL)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        
+
+let start_date = "2020-01-24";
+start_date_element.addEventListener("input", function(){
+    // 2021-04-28 string
+    start_date = start_date_element.value
+    console.log(start_date)
+    fetchData(user_country, start_date, end_date)
+})
+
+const today = new Date(Date.now());
+let end_date = today.toISOString().slice(0,10);;
+end_date_element.addEventListener("input", function(){
+    // 2021-04-28 string
+    end_date = end_date_element.value
+    console.log(end_date)
+    fetchData(user_country, start_date, end_date)
+
+})
+
+function generateCountryList(country_list, num_of_ul_lists){
+    let ul_list_id;
+
+    country_list.forEach( (country, index) => {
+        ul_list_id = `list-${index % num_of_ul_lists}` ;
+        country_list_element.innerHTML += `<ul id='${ul_list_id}'></ul>`;
+
+        document.getElementById(`${ul_list_id}`).innerHTML += `
+            <li onclick="fetchData('${country.name}', start_date, end_date)" id="${country.name}">
+            ${country.name}
+            </li>
+        `;
     })
+}
+
+generateCountryList(country_list, 3);
+
+
+change_country_btn.addEventListener("click", function(){
+    country_input.value = "";
+    clearCountryList();
+    search_country_element.classList.toggle("hide");
+    search_country_element.classList.add("fadeIn");
+});
+
+close_list_btn.addEventListener("click", function(){
+    search_country_element.classList.toggle("hide");
+});
+
+country_list_element.addEventListener("click", function(){
+    search_country_element.classList.toggle("hide");
+});
+
+country_input.addEventListener("input", function(){
+    let value = country_input.value.toUpperCase();
+
+    country_list.forEach( country => {
+        if( country.name.toUpperCase().startsWith(value)){
+            document.getElementById(country.name).classList.remove("hide");
+        }else{
+            document.getElementById(country.name).classList.add("hide");
+        }
+    })
+})
+
+
+
+function clearCountryList(){
+    country_list.forEach( country => {
+        document.getElementById(country.name).classList.remove("hide");
+    })
+}
+
+
+// fetch data
+const country_name_element = document.querySelector(".country .name");
+const total_cases_element = document.querySelector(".country-total-case .value");
+const new_cases_element = document.querySelector(".country-total-case .new-value");
+const active_element = document.querySelector(".country-active-case .value");
+const new_active_element = document.querySelector(".country-active-case .new-value");
+const deaths_element = document.querySelector(".country-death-case .value");
+const new_deaths_element = document.querySelector(".country-death-case .new-value");
+
+const ctx = document.getElementById("time-series-chart").getContext("2d");
+
+let data = [],
+    cases_list = [],
+    active_list = [],
+    deaths_list = [],
+    dates = [];
+
+
+
+function fetchData(country, start_date, end_date) {
+  user_country = country;
+  country_name_element.innerText = "Loading...";
+
+    cases_list = [],
+    active_list = [],
+    deaths_list = [],
+    dates = [],
+    formatedDates = [];
+
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  const api_fetch = async (country) => {
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country,
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          let date = entry.Date.slice(0,10);
+          if(date <= end_date && date >= start_date) {
+            dates.push(date);
+            cases_list.push(entry.Confirmed);
+            active_list.push(entry.Active);
+            deaths_list.push(entry.Deaths);
+          }
+        });
+      });
+
+
+    updateStats();
+    updateChart();
+  };
+
+  api_fetch(country);
+}
+
+fetchData(user_country, start_date, end_date);
+
+
+function updateStats() {
+  const total_cases = cases_list[cases_list.length - 1];
+  const new_confirmed_cases = total_cases - cases_list[cases_list.length - 2];
+
+  const total_active = active_list[active_list.length - 1];
+  const new_active_cases =
+    total_active - active_list[active_list.length - 2];
+
+  const total_deaths = deaths_list[deaths_list.length - 1];
+  const new_deaths_cases = total_deaths - deaths_list[deaths_list.length - 2];
+
+  country_name_element.innerHTML = user_country;
+  total_cases_element.innerHTML = total_cases || 0;
+  new_cases_element.innerHTML =new_confirmed_cases >= 0 ?  `+${new_confirmed_cases}` :  `${new_confirmed_cases}`;
+  active_element.innerHTML = total_active || 0;
+  new_active_element.innerHTML = new_active_cases >= 0 ? `+${new_active_cases}` : `${new_active_cases}`;
+  deaths_element.innerHTML = total_deaths || 0;
+  new_deaths_element.innerHTML = new_deaths_cases >= 0 ?  `+${new_deaths_cases}` : `${new_deaths_cases}`;
+
+}
+
+
+
+
+let my_chart;
+function updateChart() {
+  if (my_chart) {
+    my_chart.destroy();
+  }
+
+  my_chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      datasets: [
+        {
+          label: "Cases",
+          data: cases_list,
+          fill: false,
+          borderColor: "#fcecdd",
+          backgroundColor: "#fcecdd",
+          borderWidth: 1,
+        },
+        {
+          label: "Active",
+          data: active_list,
+          fill: false,
+          borderColor: "#fea82f",
+          backgroundColor: "#fea82f",
+          borderWidth: 1,
+        },
+        {
+          label: "Deaths",
+          data: deaths_list,
+          fill: false,
+          borderColor: "#f44336",
+          backgroundColor: "#f44336",
+          borderWidth: 1,
+        },
+      ],
+      labels: dates,
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      layout: {
+          padding:1
+      }
+    },
+  });
+}
+
+
